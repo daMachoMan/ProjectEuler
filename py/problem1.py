9cc2f13d438b123f4217913089649f32db56bd26
PJ <pkatema1@sheffield.ac.uk>
2019-01-03 12:03:30 +0000

    Problem1 completed

--- py/problem1.py
+++ py/problem1.py
@@ -1,18 +1,32 @@
 # Panashe Katema --- Last Edited 05/02/2018
 
-#Produces an array of multiplesOfFive
-n = 1
-multiples = []
-while n*5<=1000:
-    multiples.append(n*5)
-    n  = n +1
+def Multiples(max,x): #Creates an array of the multiples of x up to the max value
+    y=1
+    maxNotReached = True
+    multiList=[]
+    while maxNotReached:
+        a = x*y
+        if a < max:
+            multiList.append(a)
+        else:
+            maxNotReached = False
+        y+=1
+    return multiList
+#--------------------------------------
+def ArrayDiff(a1,a2): #Combines 2 arrays and removes duplicates.
+    combinedList=a1
+    for i in a2:
+        if i not in a1:
+            combinedList.append(i)
+    return sorted(combinedList)
+#--------------------------------------
+n1=int(input("Find multiples of...  "))
+n2=int(input("And... "))
+maxNumber=int(input("Up to... "))
 
-#produces an array of multiples of three
-m = 1
-while m*3<=1000:
-    if (m*3)/5 != 0:
-        multiples.append(m*3)
-    m = m+1
-print(multiples.sorted())
+multiA = Multiples(maxNumber,n1)
+multiB = Multiples(maxNumber,n2)
 
-print(sum(multiples))
+MultiAorB = ArrayDiff(multiA,multiB)
+
+print(sum(MultiAorB))
